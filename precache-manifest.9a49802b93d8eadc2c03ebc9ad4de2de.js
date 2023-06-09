61edcb478d89cb5d4ae54e1f166aa2132f1f8474
Code Sandbox <hello@codesandbox.io>
2023-06-09 14:19:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9a49802b93d8eadc2c03ebc9ad4de2de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aee71571faaa90d809bc087987f264f",
+    "url": "https://sneha1505.github.io/csb-nzf9mw/index.html"
+  },
+  {
+    "revision": "ace69533b345721aaa38",
+    "url": "https://sneha1505.github.io/csb-nzf9mw/static/css/main.1e39348c.chunk.css"
+  },
+  {
+    "revision": "18760cbb47702eea8a19",
+    "url": "https://sneha1505.github.io/csb-nzf9mw/static/js/2.b51b572f.chunk.js"
+  },
+  {
+    "revision": "0d7ee89e77fede7369e63c7130f11b30",
+    "url": "https://sneha1505.github.io/csb-nzf9mw/static/js/2.b51b572f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace69533b345721aaa38",
+    "url": "https://sneha1505.github.io/csb-nzf9mw/static/js/main.0e741c54.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://sneha1505.github.io/csb-nzf9mw/static/js/main.0e741c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d95c288cb2df2a3113",
+    "url": "https://sneha1505.github.io/csb-nzf9mw/static/js/runtime-main.0f1a8861.js"
+  }
+]);
